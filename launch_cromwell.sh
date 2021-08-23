590f0e7565cbab94d74c23493790f26d4d99ac3a
monkollek <monkol.lek@yale.edu>
2021-08-23 19:18:00 -0400

    copy over and edit from cromwell_wdl repo

---
+++ launch_cromwell.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -n 1
+#SBATCH -c 4
+#SBATCH -J launch_cromwell
+#SBATCH --mem=16000
+
+java -Dconfig.file=wdl/slurm.conf -jar \
+/gpfs/ycga/project/lek/shared/tools/jars/cromwell-56.jar run \
+wdl/Multisample_Fastq_to_Bam_GATK4.wdl \
+-i wdl/Multisample_Fastq_to_Bam_GATK4_inputs_hg38.json \
+-o wdl/cromwell.options
